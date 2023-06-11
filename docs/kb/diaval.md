bbc446be9c6896995213b2839995a747d5387892
Holydoom Hootsman <marukubaka@gmail.com>
2023-06-11 05:45:18 +0300

    fix hook loop

--- docs/kb/diaval.md
+++ docs/kb/diaval.md
@@ -1,10 +1,15 @@
 # Dia Shop Values { .red-font }
 
+!!! warning "CONSIDERATIONS"
+
+        _EG.Yellow_, _Juice_ based on refresh shop cost, local price omitted from calculations since gold and fodder eventually became useless  
+        _TC, POE_ costs average values of all income places  
+        Rift values of different prices **same** resource averaged  
 <!-- {{ read_excel('../assets/tables/AFKDiavalues.xlsx', engine='openpyxl', na_filter=False) }} -->
 ## Dia Prices
 
-{{ read_csv('../assets/tables/val1.csv', na_filter=True, keep_default_na=False, skip_blank_lines=False,  colalign=("center",)) }}
+{{ read_csv('../assets/tables/val.csv', na_filter=True, keep_default_na=False, skip_blank_lines=False,  colalign=("center",)) }} 
 
 ## Local Prices
 
-{{ read_csv('../assets/tables/local1.csv', na_filter=True, keep_default_na=False, skip_blank_lines=False,  colalign=("center",)) }}
+{{ read_csv('../assets/tables/local.csv', na_filter=True, keep_default_na=False, skip_blank_lines=False,  colalign=("center",)) }} 
--- scripts/hooks.py
+++ scripts/hooks.py
@@ -1,28 +1,26 @@
-# import os
 from io import BytesIO
 
 import pandas as pd
-import requests
 
 
-def on_pre_build(config, **kwargs) -> None:
-    # if os.environ['disable_hook'] == 1: return None
+def on_startup(**kwargs) -> None:
     spreadsheet_id = "1_L4LmobsOtmVeBi3RwTCespyMq4vZLSJT1E-QOsXpoY"
-    sheet_ids = [156134846, 249616100]
-    url = f"https://docs.google.com/spreadsheets/d/{spreadsheet_id}/export?gid={sheet_ids[0]}&format=csv"
-    url2 = f"https://docs.google.com/spreadsheets/d/{spreadsheet_id}/export?gid={sheet_ids[1]}&format=csv"
-    # r = requests.get('https://docs.google.com/spreadsheet/ccc?key=1_L4LmobsOtmVeBi3RwTCespyMq4vZLSJT1E-QOsXpoY&output=csv&gid=')
-    # data = r.content
-    # df = pd.read_csv(BytesIO(data),na_filter=True, keep_default_na=True, skip_blank_lines=True, index_col=0, usecols=[0,1,2,3,4])
-    df = pd.read_csv(url, index_col=0)
-    df.to_csv("docs/assets/tables/val1.csv")   
+    sheet_ids = {"val": 156134846, "loc": 249616100}
+    for k,v  in sheet_ids.items():
+        url = f"https://docs.google.com/spreadsheets/d/{spreadsheet_id}/export?gid={v}&format=csv"
+        df = pd.read_csv(url, index_col=0)
+        savedir = f"docs/assets/tables/{k}.csv"
+        df.to_csv(savedir)   
+        
+        
+    # url2 = f"https://docs.google.com/spreadsheets/d/{spreadsheet_id}/export?gid={sheet_ids[1]}&format=csv"
      
-    # r2 = requests.get('https://docs.google.com/spreadsheet/ccc?key=1_L4LmobsOtmVeBi3RwTCespyMq4vZLSJT1E-QOsXpoY&output=csv&gid=')
-    # data2 = r.content
-    # df2 = pd.read_csv(BytesIO(data2),na_filter=True, keep_default_na=True, skip_blank_lines=True, index_col=0, usecols=[0,1,2,3,4])
-    df2 = pd.read_csv(url2, index_col=0)
-    df2.to_csv("docs/assets/tables/local1.csv")
+    # df2 = pd.read_csv(url2, index_col=0)
+    # df2.to_csv("docs/assets/tables/local.csv")
     
 # https://docs.google.com/spreadsheets/d/1_L4LmobsOtmVeBi3RwTCespyMq4vZLSJT1E-QOsXpoY/edit?usp=sharing
-
+# r2 = requests.get('https://docs.google.com/spreadsheet/ccc?key=1_L4LmobsOtmVeBi3RwTCespyMq4vZLSJT1E-QOsXpoY&output=csv&gid=')
+# df2 = pd.read_csv(BytesIO(r.content),na_filter=True, keep_default_na=True, skip_blank_lines=True, index_col=0, usecols=[0,1,2,3,4])
 # usecols=['Name', 'Amount',	'Price(AVG)','Shop(AVG)','Single']
+
+# 
