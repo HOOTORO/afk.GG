2fbbcbd25954b51c51df20c5f8f2bee8945ea070
Holydoom Hootsman <marukubaka@gmail.com>
2023-06-14 14:53:51 +0300

    working swipe lists

--- src/select.ts
+++ src/select.ts
@@ -1,5 +1,4 @@
-// render Black select
-function renderSelect() {
+function renderBalck() {
   $("select[data-menu]").each(function () {
     let select = $(this),
       options = select.find("option"),
@@ -23,54 +22,7 @@ function renderSelect() {
   });
 }
 
-$(document).on("click", ".select-menu", function (e) {
-  let menu = $(this);
-
-  if (!menu.hasClass("open")) {
-    menu.addClass("open");
-  }
-});
-
-$(document).on("click", ".select-menu > ul > li", function (e) {
-  let li = $(this),
-    menu = li.parent().parent(),
-    select = menu.children("select"),
-    selected = select.find("option:selected"),
-    index = li.index();
-
-  menu.css("--t", index * -41 + "px");
-  selected.attr("selected", 0);
-  select.find("option").eq(index).attr("selected", 1);
-
-  menu.addClass(index > selected.index() ? "tilt-down" : "tilt-up");
-
-  setTimeout(() => {
-    menu.removeClass("open tilt-up tilt-down");
-  }, 500);
-});
-
-$(document).on("click", (e) => {
-  e.stopPropagation();
-  if ($(".select-menu").has(e.target.activeElement).length === 0) {
-    $(".select-menu").removeClass("open");
-  }
-});
-
-// EL DROP
-function loadDrop() {
-  $(".dropdown-el").on("click", function (e) {
-    e.preventDefault();
-    e.stopPropagation();
-    $(this).toggleClass("expanded");
-    $("#" + $(e.target).attr("for")).prop("checked", true);
-  });
-  $(document).on("click", function () {
-    $(".dropdown-el").removeClass("expanded");
-  });
-}
-
-// render wheel
-function renderSelectTwo() {
+function renderHorizontal() {
   $("select[data-menu]").each(function () {
     let select = $(this),
       type = select.data("menu"),
@@ -93,33 +45,14 @@ function renderSelectTwo() {
   });
 }
 
-$(document).on("click", ".select-menu", function (e) {
-  let menu = $(this),
-    select = menu.children("select"),
-    options = select.find("option"),
-    active = select.find("option:selected"),
-    button = menu.children("button"),
-    buttonDiv = button.children("div"),
-    current = buttonDiv.children("span");
-
-  if (!menu.hasClass("change")) {
-    let nextOption = options.eq(
-        active.index() == options.length - 1 ? 0 : active.index() + 1
-      ),
-      next = $("<span />")
-        .addClass("next")
-        .text(nextOption.text())
-        .appendTo(buttonDiv);
-
-    options.attr("selected", 0);
-    nextOption.attr("selected", 1);
-
-    menu.addClass("change");
-
-    setTimeout(() => {
-      next.removeClass("next");
-      menu.removeClass("change");
-      current.remove();
-    }, 650);
-  }
-});
+function renderCutie() {
+  $(".dropdown-el").on("click", function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $(this).toggleClass("expanded");
+    $("#" + $(e.target).attr("for")).prop("checked", true);
+  });
+  $(document).on("click", function () {
+    $(".dropdown-el").removeClass("expanded");
+  });
+}
--- src/tablerender.ts
+++ src/tablerender.ts
@@ -1,7 +1,7 @@
 window.onload = function () {
   findTable();
   selectPriority();
-  renderSelectTwo();
+  renderHorizontal();
 };
 
 function findTable() {
@@ -20,16 +20,51 @@ function selectPriority() {
     element.innerHTML = "";
     const selectlist = document.createElement("select");
     selectlist.setAttribute("data-menu", "horizontal");
-    ["0", "1", "2", "3", "4", "5"].forEach((el) => {
+    [0, 1, 2, 3, 4, 5].forEach((el) => {
       const opt = document.createElement("option");
-      console.log(val + " <> " + el);
-      console.log(val == el);
-      if (val == el) {
+      if (val == el.toString()) {
         opt.setAttribute("selected", "");
       }
-      opt.innerHTML = el;
+      opt.innerHTML = el.toString();
       selectlist.appendChild(opt);
     });
     element.appendChild(selectlist);
   }
 }
+
+function generateTable() {
+  // creates a <table> element and a <tbody> element
+  const tg = document.getElementById("tbl");
+  const tbl = document.createElement("table");
+  const tblBody = document.createElement("tbody");
+
+  // creating all cells
+  for (let i = 0; i < 2; i++) {
+    // creates a table row
+    const row = document.createElement("tr");
+
+    for (let j = 0; j < 2; j++) {
+      // Create a <td> element and a text node, make the text
+      // node the contents of the <td>, and put the <td> at
+      // the end of the table row
+      const cell = document.createElement("td");
+      const cellText = document.createTextNode(`cell in row ${i}, column ${j}`);
+      cell.appendChild(cellText);
+      row.appendChild(cell);
+    }
+    // add the row to the end of the table body
+    tblBody.appendChild(row);
+  }
+
+  // put the <tbody> in the <table>
+  tbl.appendChild(tblBody);
+  tg.appendChild(tbl);
+  // sets the border attribute of tbl to '2'
+  tbl.setAttribute("border", "2");
+}
+
+const randomId = function (length = 6) {
+  return Math.random()
+    .toString(36)
+    .substring(2, length + 2);
+};
