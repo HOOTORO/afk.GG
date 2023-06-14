160afb42c18edd202ca4b12d98ed3da12b9e4ca4
Holydoom Hootsman <marukubaka@gmail.com>
2023-06-14 13:10:53 +0300

    cleanup

--- docs/javascripts/tablesort.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// document$.subscribe(function () {
-var tables = document.querySelectorAll("article table:not([class])");
-tables.forEach(function (table) {
-    new Tablesort(table);
-});
