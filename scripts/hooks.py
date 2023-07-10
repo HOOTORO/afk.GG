0064ae0b5ac44ae0d513ff8faa5077db9ff8d6ec
Hootsman <marukubaka@gmail.com>
2023-07-10 10:04:29 +0300

    naming conventions

--- scripts/hooks.py
+++ scripts/hooks.py
@@ -4,27 +4,26 @@ import os
 import discoworker.async_runner as dar
 import numpy as np
 import pandas as pd
-# from discoworker import async_runner
 
 
 def on_startup(**kwargs) -> None:
-scripts/hooks.py    memeSrc = "docs/theme/assets/images/meme"
-    memeTarget = "docs/src/md/memes.json"
-    mkdocsDir="docs/src/md/tbl"
+    meme_src = "docs/theme/assets/images/meme"
+    meme_target = "docs/src/md/memes.json"
+    mkdocs_dir="docs/src/md/tbl"
     spreadsheet_id = "1_L4LmobsOtmVeBi3RwTCespyMq4vZLSJT1E-QOsXpoY" # values
     files = {}
     files["memes"] = dar.getMemes()
-    if os.path.isdir(memeSrc):
-        files["memes"] += os.listdir(memeSrc)
-        with open(memeTarget, 'w') as outfile:
+    if os.path.isdir(meme_src):
+        files["memes"] += os.listdir(meme_src)
+        with open(meme_target, 'w') as outfile:
             json.dump(files, outfile)
     
-    if not os.path.isdir(mkdocsDir):
-        os.makedirs(mkdocsDir)
+    if not os.path.isdir(mkdocs_dir):
+        os.makedirs(mkdocs_dir)
         
     sheet_ids = {"val": 156134846, "loc": 249616100, "rf2p": 639889886, "HOOT":1162827151}
     for k,v  in sheet_ids.items():
         url = f"https://docs.google.com/spreadsheets/d/{spreadsheet_id}/export?gid={v}&format=csv"
         df = pd.read_csv(url, index_col=0,na_filter=True, keep_default_na=False, engine='c', dtype={'Diamonds': np.string_})
-        savedir = f"{mkdocsDir}/{k}.csv"
-        df.to_csv(savedir)
+        save_dir = f"{mkdocs_dir}/{k}.csv"
+        df.to_csv(save_dir)
